e605fbb808ee99f0fdd9e315d85a755b603038c8
abdullo <goldabdullo@gmail.com>
2023-11-22 21:43:13 +0500

    01

---
+++ script/main.js
@@ -0,0 +1,59 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollImages = document.querySelector(".scroll-images");
+    const scrollLength = scrollImages.scrollWidth - scrollImages.clientWidth;
+    const leftButton = document.querySelector(".left");
+    const rightButton = document.querySelector(".right");
+  
+    function checkScroll() {
+      const currentScroll = scrollImages.scrollLeft;
+      if (currentScroll === 0) {
+        leftButton.setAttribute("disabled", "true");
+        rightButton.removeAttribute("disabled");
+      } else if (currentScroll === scrollLength) {
+        rightButton.setAttribute("disabled", "true");
+        leftButton.removeAttribute("disabled");
+      } else {
+        leftButton.removeAttribute("disabled");
+        rightButton.removeAttribute("disabled");
+      }
+    }
+  
+    scrollImages.addEventListener("scroll", checkScroll);
+    window.addEventListener("resize", checkScroll);
+    checkScroll();
+  
+    function leftScroll() {
+      scrollImages.scrollBy({
+        left: -200,
+        behavior: "smooth"
+      });
+    }
+  
+    function rightScroll() {
+      scrollImages.scrollBy({
+        left: 200,
+        behavior: "smooth"
+      });
+    }
+  
+    leftButton.addEventListener("click", leftScroll);
+    rightButton.addEventListener("click", rightScroll);
+  });
